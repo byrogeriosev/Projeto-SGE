85fc2d7b8c161e2c69bdc12f4798a4b9ea48a963
byrogeriosev <junior.rogerio3828@gmail.com>
2024-10-24 15:29:17 -0300

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const Sequelize = new Sequelize('sge_js', 'root', 'root', {
- host: 'localhost',
- dialect: 'mysql',
-});
-
-module.exports = Sequelize;
