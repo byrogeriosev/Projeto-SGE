c66c5f244d6119f2e0994d56a4eea80607bd5c01
byrogeriosev <junior.rogerio3828@gmail.com>
2024-10-31 08:44:13 -0300

    Delete pedidoRoutes.js

--- pedidoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pedidoController = require('../controllers/pedidoController')
-
-router.get('/pedidos', pedidoController.criarPedido)
-router.get('/pedidos', pedidoController.listarPedidos)
-
-module.exports = router;
